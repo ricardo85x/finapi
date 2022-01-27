a423d5f3a38362aa65089d2d1ef854858386f9d5
Ricardo <ricardoinliner@yahoo.com>
2022-01-27 10:55:07 -0300

    feat - create account

--- src/index.js
+++ src/index.js
@@ -1,7 +1,31 @@
 const express = require('express');
+const { v4 : uuidv4  } = require('uuid')
+
 const app = express();
 
 app.use(express.json())
+
+
+/**
+ * cpf - string
+ * name - string
+ * id - uuid
+ * statement - []
+ */
+
+
+const customers = []
+
+app.post("/account", (req, res) => {
+    const { cpf, name } = req.body
+    const id = uuidv4()
+
+    customers.push({
+        cpf, name, id, statement: []
+    })
+
+    return res.status(201).send()
+})
 
 app.get('/', (req, res) => {
     return res.json({ message: 'Hello, world!' });
